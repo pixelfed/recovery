22c91aa87d7129e85bed21d985c04e94eb2cc876
Daniel Supernault <danielsupernault@gmail.com>
2023-07-25 01:34:57 -0600

    Add hCaptcha

--- config/config.php
+++ config/config.php
@@ -1,5 +1,16 @@
 <?php
 
 return [
-    'token' => env('RECOVERY_APP_TOKEN', '069fc8a0-8e5a-417b-9cfa-0e97db1cbc73')
+    'token' => env('RECOVERY_APP_TOKEN', '069fc8a0-8e5a-417b-9cfa-0e97db1cbc73'),
+
+    'captcha' => [
+        'enabled' => env('RECOVERY_CAPTCHA_ENABLED', false),
+        'sitekey' => env('RECOVERY_CAPTCHA_SITEKEY', ''),
+        'secret' => env('RECOVERY_CAPTCHA_SECRET', '')
+    ],
+
+    'rate_limit' => [
+        'ttl' => env('RECOVERY_RATELIMIT_TTL', 14400),
+        'attempts' => env('RECOVERY_RATELIMIT_ATTEMPTS', 10),
+    ],
 ];
